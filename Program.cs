0f2bc262b71cd89e76e879beeed6927519f8189b
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-01-08 15:09:56 +0100

    Flyttat till mappar, uppdaterat todo-list, queries, gitignore och diagram, exporterat diagram till png

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+static class Menu
+{
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (IDbConnection db = DatabaseFunctions.Connect())
+        {
+            List<Person> people = db.Query<Person>("SELECT * FROM Person").ToList();
+            UI.PrintInTable(people);
+        }
+    }
+}
+
+
+
