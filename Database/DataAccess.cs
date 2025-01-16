a7401455cafdb4e290053fd1f82087f045ec7dba
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-01-16 12:24:48 +0100

    lagt till metoder för att skriva in vad man vill hämta från DataAccess-klassen, och utskrift av resultatet Lagt till metoder med alla queries som hämtar data från sql-databasen

--- Database/DataAccess.cs
+++ Database/DataAccess.cs
@@ -9,31 +9,205 @@ class DatabaseFunctions
         string connectionString = File.ReadAllText("connectionString.txt");
         return new SqlConnection(connectionString);
     }
-
-    public IEnumerable<T> Select<T, U>(string storedProcedure, U parameters)
+    public static IEnumerable<CrimeCity> CrimeByDateAndCity(string date, string city)
     {
         using IDbConnection db = Connect();
-        return db.Query<T>(storedProcedure, parameters, commandType: CommandType.Text);
-        //($"SELECT {parameters} FROM {typeof(T).Name}").ToList();
+        
+        string query = @"
+            SELECT c.Name, c.Date, ci.Name AS CityName
+            FROM Crime c
+            JOIN City ci ON c.CityId = ci.Id
+            WHERE c.Date = @Date AND ci.Name = @CityName";
+        
+        return db.Query<CrimeCity>(query, new { Date = date, CityName = city });
     }
-
-    public void Insert<T, U>(string storedProcedure, U parameters)
+    public static IEnumerable<CriminalsByGang> CriminalsByGang(string gangName)
     {
         using IDbConnection db = Connect();
-        db.Execute($"INSERT INTO {typeof(T).Name} VALUES (@{parameters})", parameters);
-    }
+        
+        string query = @"
+            SELECT p.Name, p.DateOfBirth, c.Description AS PersonDescription, c.Status, g.Name AS Gang, g.Description AS GangDescription
+            FROM Person p
+            JOIN Criminal c ON p.Id = c.PersonId
+            JOIN Gang g ON c.GangId = g.Id AND g.Name LIKE @GangName";
 
-    public void Update<T, U>(string storedProcedure, U parameters)
+        return db.Query<CriminalsByGang>(query, new { GangName = gangName });
+    }
+    public static IEnumerable<ActiveGangsByCity> ActiveGangsByCity(string cityName)
     {
         using IDbConnection db = Connect();
-        db.Execute($"UPDATE {typeof(T).Name} SET @{parameters} WHERE Id = @{parameters}", parameters);
-    }
+        
+        string query = @"
+            SELECT g.Name AS Gang, City.Name AS ActiveCity, g.Description AS GangDescription
+            FROM Person p
+            JOIN Criminal c ON p.Id = c.PersonId
+            JOIN Gang g ON g.Id = c.GangId
+            JOIN GangToCity GToC ON g.Id = GToC.GangId
+            JOIN City ON GToC.CityId = City.Id AND City.Name = @CityName";
 
-    public void Delete<T, U>(string storedProcedure, U parameters)
+        return db.Query<ActiveGangsByCity>(query, new { CityName = cityName });
+    }
+    public static IEnumerable<CriminalRecordsByCriminal> CriminalRecordsByCriminal(string criminalName)
     {
         using IDbConnection db = Connect();
-        db.Execute($"DELETE FROM {typeof(T).Name} WHERE Id = @{parameters}", parameters);
-    }
+        
+        string query = @"
+            SELECT i.Name, i.Description, co.Sentence, Crime.CrimeDate, co.ConvictionDate
+            FROM Criminal c
+            JOIN CrimeRecord cr ON cr.CriminalId = c.Id
+            JOIN Person p ON p.Id = c.PersonId AND p.Name LIKE @CriminalName
+            JOIN CriminalToInvestigation CtI ON CtI.CriminalId = c.Id
+            JOIN Investigation i ON i.Id = CtI.InvestigationId
+            JOIN Crime ON Crime.Id = i.CrimeId
+            JOIN Conviction co ON co.InvestigationId = i.Id";
 
-    
+        return db.Query<CriminalRecordsByCriminal>(query, new { CriminalName = criminalName });
+    }
+    public static IEnumerable<InvestigationsByCriminal> ActiveInvestigationsByCriminal(string criminalName)
+    {
+        using IDbConnection db = Connect();
+        
+        string query = @"
+            SELECT i.Name, i.Description, cr.CrimeDate, ci.Name AS City
+            FROM Investigation i
+            JOIN Crime cr ON cr.Id = i.CrimeId
+            JOIN City ci ON ci.Id = cr.CityId
+            JOIN CriminalToInvestigation CtI ON CtI.InvestigationId = i.Id 
+            JOIN Criminal c ON c.Id = CtI.CriminalId
+            JOIN Person p ON p.Id = c.PersonId
+            WHERE i.[Status] = 'Ongoing' AND p.Name LIKE @CriminalName";
+
+        return db.Query<InvestigationsByCriminal>(query, new { CriminalName = criminalName });
+    }
+    public static IEnumerable<InvestigationsByCop> ActiveInvestigationsByCop(string copName)
+    {
+        using IDbConnection db = Connect();
+        
+        string query = @"
+            SELECT i.Name, i.Description, cr.CrimeDate, ci.Name AS City
+            FROM Investigation i
+            JOIN Crime cr ON cr.Id = i.CrimeId
+            JOIN City ci ON ci.Id = cr.CityId
+            JOIN CopToInvestigation CtI ON CtI.InvestigationId = i.Id 
+            JOIN Cop c ON c.Id = CtI.CopId
+            JOIN Person p ON p.Id = c.PersonId
+            WHERE i.[Status] = 'Ongoing' AND p.Name LIKE @CopName";
+
+        return db.Query<InvestigationsByCop>(query, new { CopName = copName });
+    }
+    public static IEnumerable<InvestigationsByCriminal> AllInvestigationsByCriminal(string criminalName)
+    {
+        using IDbConnection db = Connect();
+        
+        string query = @"
+            SELECT i.Name, i.Description, cr.CrimeDate, ci.Name AS City, i.Status
+            FROM Investigation i
+            JOIN Crime cr ON cr.Id = i.CrimeId
+            JOIN City ci ON ci.Id = cr.CityId
+            JOIN CriminalToInvestigation CtI ON CtI.InvestigationId = i.Id 
+            JOIN Criminal c ON c.Id = CtI.CriminalId
+            JOIN Person p ON p.Id = c.PersonId
+            WHERE p.Name LIKE @CriminalName";
+
+        return db.Query<InvestigationsByCriminal>(query, new { CriminalName = criminalName });
+    }
+    public static IEnumerable<InvestigationsByCop> AllInvestigationsByCop(string copName)
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT i.Name, i.Description, cr.CrimeDate, ci.Name AS City, i.Status
+            FROM Investigation i
+            JOIN Crime cr ON cr.Id = i.CrimeId
+            JOIN City ci ON ci.Id = cr.CityId
+            JOIN CopToInvestigation CtI ON CtI.InvestigationId = i.Id 
+            JOIN Cop c ON c.Id = CtI.CopId
+            JOIN Person p ON p.Id = c.PersonId
+            WHERE p.Name LIKE @CopName";
+
+        return db.Query<InvestigationsByCop>(query, new { CopName = copName });
+    }
+    public static IEnumerable<GangRelations> GangRelations()
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT g1.Name AS Gang1, gc.RelationType, g2.Name AS Gang2
+            FROM GangConnections gc
+            JOIN Gang g1 ON gc.GangId1 = g1.Id
+            JOIN Gang g2 ON  gc.GangId2 = g2.Id";
+
+        return db.Query<GangRelations>(query);
+    }
+    public static IEnumerable<CriminalInfo> CriminalInfo(string criminalName)
+    {
+        using IDbConnection db = Connect();
+        
+        string query = @"
+            SELECT p.Name, p.Gender, p.DateOfBirth, cr.[Status], cr.[Description], g.Name AS Gang
+            FROM Person p 
+            JOIN Criminal cr ON cr.PersonId = p.Id
+            JOIN Gang g ON g.Id = cr.GangId
+            WHERE p.Name LIKE @CriminalName";
+
+        return db.Query<CriminalInfo>(query, new { CriminalName = criminalName });
+    }
+    public static IEnumerable<GangInfo> GangInfo()
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT g.Name, COUNT(c.Name) AS ActiveCitys, COUNT(p.Name) AS Members
+            FROM Person p 
+            JOIN Criminal cr ON cr.PersonId = p.Id
+            JOIN Gang g ON g.Id = cr.GangId
+            JOIN GangToCity GtC ON GtC.GangId = g.Id
+            JOIN City c ON c.Id = GtC.CityId
+            GROUP BY g.Name";
+
+        return db.Query<GangInfo>(query);
+    }
+    public static IEnumerable<GangAmmountByCity> GangAmmountByCity()
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT c.Name, COUNT(g.Name) AS ActiveGangs
+            FROM Gang g
+            JOIN GangToCity GtC ON GtC.GangId = g.Id
+            JOIN City c ON c.Id = GtC.CityId
+            GROUP BY c.Name";
+
+        return db.Query<GangAmmountByCity>(query);
+    }
+    public static IEnumerable<GangCriminalAmmount> GangCriminalAmmount()
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT g.Name, COUNT(p.Name) AS Members
+            FROM Person p 
+            JOIN Criminal c ON c.PersonId = p.Id
+            JOIN Gang g ON g.Id = c.GangId
+            GROUP BY g.Name";
+
+        return db.Query<GangCriminalAmmount>(query);
+    }
+    public static IEnumerable<ConvictionByCriminal> CrimeRecordAmmountByCriminal()
+    {
+        using IDbConnection db = Connect();
+
+        string query = @"
+            SELECT p.Name, COUNT(cr.Id) AS Convictions          
+            FROM Criminal c
+            JOIN CrimeRecord cr ON cr.CriminalId = c.Id
+            JOIN Person p ON p.Id = c.PersonId
+            JOIN CriminalToInvestigation CtI ON CtI.CriminalId = c.Id
+            JOIN Investigation i ON i.Id = CtI.InvestigationId
+            JOIN Crime ON Crime.Id = i.CrimeId
+            JOIN Conviction co ON co.InvestigationId = i.Id
+            GROUP BY p.Name";
+
+        return db.Query<ConvictionByCriminal>(query);
+    } 
 }
--- Database/HandleData.cs
+++ Database/HandleData.cs
@@ -1,73 +1,177 @@
 //Lägg till klass som hanterar data, och som har metoder för att hämta data från databasen, och skriva data till databasen
 //Ska hantera alla klasser som existerar, och ha färdiga stored procedures för att hämta data från databasen
+using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
 class HandleData
 {
-    public IEnumerable<Gang> GetGangs(string gangName)
+    //Lägg till foreach loop för att skriva ut alla klasser i databasen, möjligtvis en Print() metod för varje klass?
+    public static IDbConnection Connect()
     {
-        using (var db = DatabaseFunctions.Connect())
+        string connectionString = File.ReadAllText("connectionString.txt");
+        return new SqlConnection(connectionString);
+    }
+    public static void CrimeByDateAndCity()
+    {
+        Console.WriteLine("Enter date");
+        string date = Console.ReadLine();
+        Console.WriteLine("Enter city");
+        string city = Console.ReadLine();
+        IEnumerable<CrimeCity> crimeCity = DatabaseFunctions.CrimeByDateAndCity(date, city);
+        foreach (var item in crimeCity)
         {
-
-            var query = @"
-                SELECT g.Name, COUNT(c.Name) AS ActiveCitys, COUNT(p.Name) AS Members
-                FROM Person p
-                JOIN Criminal cr ON cr.PersonId = p.Id
-                JOIN Gang g ON g.Id = cr.GangId
-                JOIN GangToCity GtC ON GtC.GangId = g.Id
-                JOIN City c ON c.Id = GtC.CityId
-                WHERE g.Name LIKE '%@GangName%'
-                GROUP BY g.Name";
-
-            var parameters = new { GangName = gangName };
-            var gang = db.Query<string gangName, int citys, int members>(query, parameters);
-            return gang;
+            item.Print();
+            Console.WriteLine();
         }
+        Console.ReadKey();
+    }
+    public static void CriminalsByGang()
+    {
+        Console.WriteLine("Enter gang name");
+        string gangName = Console.ReadLine();
+        IEnumerable<CriminalsByGang> criminalGang = DatabaseFunctions.CriminalsByGang(gangName);
+        foreach (var item in criminalGang)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void ActiveGangsByCity()
+    {
+        Console.WriteLine("Enter city name");
+        string cityName = Console.ReadLine();
+        IEnumerable<ActiveGangsByCity> gangsByCities = DatabaseFunctions.ActiveGangsByCity(cityName);
+        foreach (var item in gangsByCities)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void CriminalRecordsByCriminal()
+    {
+        Console.WriteLine("Enter criminal name");
+        string criminalName = Console.ReadLine();
+        IEnumerable<CriminalRecordsByCriminal> criminalRecords = DatabaseFunctions.CriminalRecordsByCriminal(criminalName);
+        foreach (var item in criminalRecords)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void ActiveInvestigationsByCriminal()
+    {
+        Console.WriteLine("Enter criminal name");
+        string name = Console.ReadLine();
+        IEnumerable<InvestigationsByCriminal> activeCriminalInvestigations = DatabaseFunctions.ActiveInvestigationsByCriminal(name);
+        foreach (var item in activeCriminalInvestigations)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void ActiveInvestigationsByCop()
+    {
+        Console.WriteLine("Enter cop name");
+        string copName = Console.ReadLine();
+        IEnumerable<InvestigationsByCop> activeCopInvestigations = DatabaseFunctions.ActiveInvestigationsByCop(copName);
+        foreach (var item in activeCopInvestigations)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void AllInvestigationsByCriminal()
+    {
+        Console.WriteLine("Enter criminal name");
+        string nameCriminal = Console.ReadLine();
+        IEnumerable<InvestigationsByCriminal> allCriminalInvestigations = DatabaseFunctions.AllInvestigationsByCriminal(nameCriminal);
+        foreach (var item in allCriminalInvestigations)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void AllInvestigationsByCop()
+    {
+        Console.WriteLine("Enter cop name");
+        string nameCop = Console.ReadLine();
+        IEnumerable<InvestigationsByCop> allCopInvestigations = DatabaseFunctions.AllInvestigationsByCop(nameCop);
+        foreach (var item in allCopInvestigations)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void GangRelations()
+    {
+        IEnumerable<GangRelations> gangRelations = DatabaseFunctions.GangRelations();
+        foreach (var item in gangRelations)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void CriminalInfo()
+    {
+        Console.WriteLine("Enter criminal name");
+        string input = Console.ReadLine();
+        IEnumerable<CriminalInfo> criminalInfo = DatabaseFunctions.CriminalInfo(input);
+        foreach (var item in criminalInfo)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void GangInfo()
+    {
+        IEnumerable<GangInfo> gangInfo = DatabaseFunctions.GangInfo();
+        foreach (var item in gangInfo)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void GangAmmountByCity()
+    {
+        IEnumerable<GangAmmountByCity> gangCitys = DatabaseFunctions.GangAmmountByCity();
+        foreach (var item in gangCitys)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void GangCriminalAmmount()
+    {
+        IEnumerable<GangCriminalAmmount> gangMembers = DatabaseFunctions.GangCriminalAmmount();
+        foreach (var item in gangMembers)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+    public static void CrimeRecordAmmountByCriminal()
+    {
+        IEnumerable<ConvictionByCriminal> convictions = DatabaseFunctions.CrimeRecordAmmountByCriminal();
+        foreach (var item in convictions)
+        {
+            item.Print();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
     }
 
-    public IEnumerable<Person> GetPeople()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.People.ToList();
-        }
-    }
-
-    public IEnumerable<Crime> GetCrimes()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.Crimes.ToList();
-        }
-    }
-
-    public IEnumerable<Criminal> GetCriminals()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.Criminals.ToList();
-        }
-    }
-
-    public IEnumerable<City> GetCities()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.Cities.ToList();
-        }
-    }
-
-    public IEnumerable<Cop> GetCops()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.Cops.ToList();
-        }
-    }
-
-    public IEnumerable<CrimeRecord> GetCrimeRecords()
-    {
-        using (var db = DatabaseFunctions.Connect())
-        {
-            return db.CrimeRecords.ToList();
-        }
-    }   
 }
